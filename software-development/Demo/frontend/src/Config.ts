41bb8c7c12a5043557bae482e86ee41bbbe01671
Weidi Yan <126424796+wyan170@users.noreply.github.com>
2025-06-11 00:52:00 +1200

    1

--- software-development/Demo/frontend/src/Config.ts
+++
@@ -1,5 +0,0 @@
-const config = {
-    apiUrl: 'http://localhost:5006/api/students',
-  };
-  
-  export default config;
--- software-development/Demo/frontend/src/Models/Students.ts
+++
@@ -1,7 +0,0 @@
-export interface Students {
-    id: number;
-    firstName: string;
-    lastName: string;
-    email: string;
-    university: string;
-}
